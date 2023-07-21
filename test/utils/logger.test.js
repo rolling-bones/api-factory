07b45ee41f6f2b601a74ee49a04cf9a01c1e533c
sereigh <kb9286@gmail.com>
2023-07-21 17:47:10 -0600

    add tests

---
+++ test/utils/logger.test.js
@@ -0,0 +1,33 @@
+const utils = require('../../utils');
+
+it('should print the correct log message', () => {
+  const logSpy = jest.spyOn(utils.logger, 'log');
+
+  utils.logger.log('hello');
+
+  expect(logSpy).toHaveBeenCalledWith('hello');
+});
+
+it('should print the correct info message', () => {
+  const logSpy = jest.spyOn(utils.logger, 'info');
+
+  utils.logger.info('hello');
+
+  expect(logSpy).toHaveBeenCalledWith('hello');
+});
+
+it('should print the correct warn message', () => {
+  const logSpy = jest.spyOn(utils.logger, 'warn');
+
+  utils.logger.warn('hello');
+
+  expect(logSpy).toHaveBeenCalledWith('hello');
+});
+
+it('should print the correct error message', () => {
+  const logSpy = jest.spyOn(utils.logger, 'error');
+
+  utils.logger.error('hello');
+
+  expect(logSpy).toHaveBeenCalledWith('hello');
+});
