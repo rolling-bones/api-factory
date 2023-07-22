2878d8c877cc5ea39d994fdc7734f176a7094563
sereigh <kb9286@gmail.com>
2023-07-22 16:47:04 -0600

    update scripts

--- server/test/server/api.test.js
+++ server/test/server/api.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const baseURL = "http://localhost:5000";
 
 describe("GET requests", () => {
+  // TODO: make sure server is running before tests run
   it("should return 200 when /bob is requested", async () => {
     const response = await request(baseURL).get("/bob");
     expect(response.statusCode).toBe(200);
